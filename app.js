4b650a5ebcd382bd7db195973cd6def9d1762206
Victoria Gomez-Small <gomez-small.v@northeastern.edu>
2023-11-20 20:52:31 -0500

    kanbas database and service functions

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ import express from 'express'
 import Hello from "./hello.js"
 import Lab5 from "./Lab5.js"
 import cors from "cors"
+import "dotenv/config"
+// import session from "express-session";
+import CourseRoutes from './courses/routes.js'
+import ModuleRoutes from './modules/routes.js'
 const app = express()
 app.use(express.json())
 app.use(cors());
+ModuleRoutes(app)
+CourseRoutes(app)
 Hello(app)
 Lab5(app)
-app.listen(4000)
+app.listen(process.env.PORT || 4000)
 
